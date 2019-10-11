2b2a91ad82d1414b1591b9d949161957ad01ee7b
Lesly Tello <lesly.tello@epn.edu.ec>
2019-10-10 19:11:46 -0500

    Version 0.9 pantalla principal

---
+++ Principal/Principal/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    class Cliente
+    {
+        int idCliente;
+        String nombre, apellido, cedula, direccion;
+
+        public Cliente(String nombre, String apellido, String direccion)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+        }
+    }
+}
