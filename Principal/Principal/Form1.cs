4ae26b6c83611a1322b44a2012778d6068deb5ff
Lesly Tello <lesly.tello@epn.edu.ec>
2019-10-11 11:01:42 -0500

    Registro de cliente

--- Principal/Principal/Form1.cs
+++ Principal/Principal/Form1.cs
@@ -16,5 +16,18 @@ namespace Principal
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String n1 = textBox1.Text;
+            String n2 = textBox2.Text;
+            String n3 = textBox3.Text;
+            String n4 = textBox4.Text;
+
+            Cliente c = new Cliente(n1, n2, n3, n4);
+            c.insertarCliente(c);
+
+        }
     }
 }
